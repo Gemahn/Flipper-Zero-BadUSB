f8f29f7ab5f92a38d596b3c20b56c90e31c5edae
Gemahn <167044775+Gemahn@users.noreply.github.com>
2024-04-16 23:24:54 -0500

    BTTplaylist.ps1

--- Payloads/Flip-Subscribe/Subscribe.ps1
+++ Payloads/Flip-Subscribe/Subscribe.ps1
@@ -1,7 +1,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 $o=New-Object -ComObject WScript.Shell
 $url = -join($channel,"?sub_confirmation=1")
-Start-Process "$url" 
+Start-Process "https://www.youtube.com/watch?v=-HCzkLwWV1U&list=PLoptmvwrdPlt8sIx8VeiQmeRhQ1jH4vo0&index=1" 
 Start-Sleep -Seconds 5
 [System.Windows.Forms.SendKeys]::SendWait('{TAB}'*2)
 [System.Windows.Forms.SendKeys]::SendWait('{ENTER}')
